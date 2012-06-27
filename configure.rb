bfbb32a671a6e58cd9f530fdf41d25f6880936bc
Andrew <andrew.k.jordan@gmail.com>
2012-06-26 17:02:23 -0700

    tweaks

--- configure.rb
+++ configure.rb
@@ -1,7 +1,6 @@
 ENV['TWILIO_ACCOUNT_SID'] = 'XXXXXXXXXXX'
 ENV['TWILIO_AUTH_TOKEN'] = 'XXXXXXXXXXX'
 ENV['TWILIO_NUMBER'] = 'XXXXXXXXXXX'
-ENV['PG_CREDENTIALS'] = 'XXXXXXXXXXX'
 ENV['RACK_USER'] = 'XXXXXXXXXXX'
 ENV['RACK_PW'] = 'XXXXXXXXXXX'
 
@@ -10,7 +9,6 @@ puts "Please copy paste these into your shell to test locally:\n" +
       "export TWILIO_ACCOUNT_SID=#{ENV['TWILIO_ACCOUNT_SID']}\n" +
       "export TWILIO_AUTH_TOKEN=#{ENV['TWILIO_AUTH_TOKEN']}\n" +
       "export TWILIO_NUMBER=#{ENV['TWILIO_NUMBER']}\n" +
-      "export PG_CREDENTIALS=#{ENV['PG_CREDENTIALS']}\n" +
       "export RACK_USER=#{ENV['RACK_USER']}\n" +
       "export RACK_PW=#{ENV['RACK_PW']}\n"
 
@@ -19,6 +17,5 @@ puts "Please copy paste these into your shell to test locally:\n" +
 system("heroku config:add TWILIO_ACCOUNT_SID=#{ENV['TWILIO_ACCOUNT_SID']} " \
       "TWILIO_AUTH_TOKEN=#{ENV['TWILIO_AUTH_TOKEN']} " \
       "TWILIO_NUMBER=#{ENV['TWILIO_NUMBER']} " \
-      "PG_CREDENTIALS=#{ENV['PG_CREDENTIALS']} " \
       "RACK_USER=#{ENV['RACK_USER']} " \
       "RACK_PW=#{ENV['RACK_PW']} ")
